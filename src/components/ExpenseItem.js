55029a8a42ebb50afa7a09a2707711594c36a9ba
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-01 21:11:37 +0530

    props is created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2022, 3, 28);
     const expenseInsurance = 'Car Insurance';
     const expensePrice = 294.67
   
     return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseInsurance}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
