e7686170221d6905faf6c6236f813d51c1d52c3c
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-02 13:29:32 +0530

    props is created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2022, 3, 28);
-    const expenseInsurance = 'Car Insurance';
-    const expensePrice = 294.67
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('es-US', {day: '2-digit'} );
+    const year = props.date.getFullYear();
   
     return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
