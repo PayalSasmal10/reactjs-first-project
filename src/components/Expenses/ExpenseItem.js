740f01325e59efda61a069a3734bd745b528fce3
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-03 22:03:43 +0530

    Form is about to create

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -24,7 +24,7 @@ const  ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={btnClick}>Click Me</button>
+      {/* <button onClick={btnClick}>Click Me</button> */}
     </Card>
   );
 }
