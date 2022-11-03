8b724083b0cae5dc17e12c59d8b5d6052b3d4f0c
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-03 13:22:25 +0530

    props is created

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,10 +7,10 @@ function ExpenseDate(props){
     const year = props.date.getFullYear();
     
     return (
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+      <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__day">{day}</div>
+        <div className="expense-date__year">{year}</div>
       </div>
     );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from './Card';
 
 function ExpenseItem(props) {
   console.log("Item:",props.date);
@@ -8,13 +9,13 @@ function ExpenseItem(props) {
     // const year = props.date.getFullYear();
   
     return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
