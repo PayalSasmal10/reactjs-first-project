19d031b210cf089ac2117472e8b377d42a53c2a3
Payal Sasmal <sasmalpayal@gmail.com>
2022-11-03 21:37:16 +0530

    Chnaged folder structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,8 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
+import React, { useState } from 'react';
+
 
 const  ExpenseItem = (props) => {
   
@@ -8,15 +10,18 @@ const  ExpenseItem = (props) => {
     // const day = props.date.toLocaleString('es-US', {day: '2-digit'} );
     // const year = props.date.getFullYear();
 
+    const [title, setTitle] = useState(props.title);
+
     const btnClick = () => {
-      console.log("Clicked Me!");
+      setTitle('Updated!');
+      console.log(title);
     }
   
     return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={btnClick}>Click Me</button>
